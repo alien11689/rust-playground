9b1d2c230f8905b310bd5a0de264146a810ccab3
Dominik Przybysz <dpr@touk.pl>
2024-11-30 21:19:38 +0100

    Add go interop with rust lib

---
+++ interop/go-dynamic/go-dynamic.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+#cgo CFLAGS: -I..
+#cgo LDFLAGS: -L../multiplier-dynamic/target/release -lmultiplier -L../adder -ladder
+#include "multiplier.h"
+*/
+import "C"
+import "fmt"
+
+func main(){
+    res := C.multiply(10, -82)
+    fmt.Println("GO Dynamic")
+    fmt.Println(res)
+}
---
+++ interop/go-dynamic/go.mod
@@ -0,0 +1,3 @@
+module go-dynamic
+
+go 1.23.3
