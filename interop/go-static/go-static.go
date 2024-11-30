e577f3ddfaab424f69d054191884385a71e2cafa
Dominik Przybysz <dpr@touk.pl>
2024-11-30 21:19:38 +0100

    Add go interop with rust lib

---
+++ interop/go-static/go-static.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+#cgo CFLAGS: -I..
+#cgo LDFLAGS: -L ../multiplier-static/target/release -lmultiplier
+#include "multiplier.h"
+*/
+import "C"
+import "fmt"
+
+func main(){
+    res := C.multiply(10, -82)
+    fmt.Println("GO Static")
+    fmt.Println(res)
+}
