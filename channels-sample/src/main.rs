eb73445cca9de4681ff278899b7a4f56f1bbdf3e
Dominik Przybysz <alien11689@gmail.com>
2025-01-05 21:41:48 +0100

    Add channels sample

---
+++ channels-sample/src/main.rs
@@ -0,0 +1,63 @@
+use rand::Rng;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn run_on_threads(threads: i32, jobs: i32) -> i32 {
+    let (mut job_sender, job_receiver) = spmc::channel();
+    let (result_sender, result_receiver) = mpsc::channel();
+
+    let mut workers = Vec::new();
+    for n in 0..threads {
+        let receiver = job_receiver.clone();
+        let sender = result_sender.clone();
+        workers.push(thread::spawn(move || {
+            let mut result = 0;
+            for _ in 0..(jobs / threads) {
+                let msg = receiver.recv().unwrap();
+                let duration = Duration::from_millis(rand::rng().random_range(10..20));
+                println!(
+                    "Worker {} received: {} and sleeping for {:?}",
+                    n, msg, duration
+                );
+                result += msg;
+                thread::sleep(duration)
+            }
+            sender.send((n, result)).unwrap()
+        }));
+    }
+
+    for i in 0..jobs {
+        job_sender.send(i).unwrap();
+    }
+
+    let mut result = 0;
+    for _ in 0..threads {
+        let msg = result_receiver.recv().unwrap();
+        println!("Spawner received: {:?}", msg);
+        result += msg.1;
+    }
+
+    for worker in workers {
+        worker.join().unwrap();
+    }
+
+    result
+}
+
+fn main() {
+    let n = 1000;
+    let res = run_on_threads(50, n);
+    println!("Sum number from 0..<{n} is {res}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_sum_numbers() {
+        let n = 100;
+        assert_eq!(n * (n - 1) / 2, run_on_threads(5, n));
+    }
+}
