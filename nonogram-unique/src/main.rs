e35163a8efe9565185b429003df1c2b6819395a3
Dominik Przybysz <alien11689@gmail.com>
2025-07-27 13:40:19 +0200

    Do not allocate the rows

--- nonogram-unique/src/main.rs
+++ nonogram-unique/src/main.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 
-type Row = [bool; 5];
-
 #[derive(Hash, Eq, PartialEq, Debug)]
 struct FixSizeClue {
     len: usize,
@@ -14,20 +12,21 @@ fn get_bit(n: u32, i: u8) -> bool {
     n >> i & 1 == 1
 }
 
-fn get_clue(row: Row) -> FixSizeClue {
+fn get_clue(n: u32, idx: [u8; 5]) -> FixSizeClue {
     let mut cur = 0u8;
     let mut last = false;
     let mut res = [0u8; 5];
     let mut len = 0;
-    for x in row.iter() {
-        if *x == last {
+    for i in idx.iter() {
+        let x = get_bit(n, *i);
+        if x == last {
             cur += 1;
         } else {
             if last {
                 res[len] = cur;
                 len += 1;
             }
-            last = *x;
+            last = x;
             cur = 1;
         }
     }
@@ -40,81 +39,21 @@ fn get_clue(row: Row) -> FixSizeClue {
 
 fn get_clues(n: u32) -> Clues {
     [
-        get_clue([
-            get_bit(n, 0),
-            get_bit(n, 1),
-            get_bit(n, 2),
-            get_bit(n, 3),
-            get_bit(n, 4),
-        ]),
-        get_clue([
-            get_bit(n, 5),
-            get_bit(n, 6),
-            get_bit(n, 7),
-            get_bit(n, 8),
-            get_bit(n, 9),
-        ]),
-        get_clue([
-            get_bit(n, 10),
-            get_bit(n, 11),
-            get_bit(n, 12),
-            get_bit(n, 13),
-            get_bit(n, 14),
-        ]),
-        get_clue([
-            get_bit(n, 15),
-            get_bit(n, 16),
-            get_bit(n, 17),
-            get_bit(n, 18),
-            get_bit(n, 19),
-        ]),
-        get_clue([
-            get_bit(n, 20),
-            get_bit(n, 21),
-            get_bit(n, 22),
-            get_bit(n, 23),
-            get_bit(n, 24),
-        ]),
-        get_clue([
-            get_bit(n, 0),
-            get_bit(n, 5),
-            get_bit(n, 10),
-            get_bit(n, 15),
-            get_bit(n, 20),
-        ]),
-        get_clue([
-            get_bit(n, 1),
-            get_bit(n, 6),
-            get_bit(n, 11),
-            get_bit(n, 16),
-            get_bit(n, 21),
-        ]),
-        get_clue([
-            get_bit(n, 2),
-            get_bit(n, 7),
-            get_bit(n, 12),
-            get_bit(n, 17),
-            get_bit(n, 22),
-        ]),
-        get_clue([
-            get_bit(n, 3),
-            get_bit(n, 8),
-            get_bit(n, 13),
-            get_bit(n, 18),
-            get_bit(n, 23),
-        ]),
-        get_clue([
-            get_bit(n, 4),
-            get_bit(n, 9),
-            get_bit(n, 14),
-            get_bit(n, 19),
-            get_bit(n, 24),
-        ]),
+        get_clue(n, [0, 1, 2, 3, 4]),
+        get_clue(n, [5, 6, 7, 8, 9]),
+        get_clue(n, [10, 11, 12, 13, 14]),
+        get_clue(n, [15, 16, 17, 18, 19]),
+        get_clue(n, [20, 21, 22, 23, 24]),
+        get_clue(n, [0, 5, 10, 15, 20]),
+        get_clue(n, [1, 6, 11, 16, 21]),
+        get_clue(n, [2, 7, 12, 17, 22]),
+        get_clue(n, [3, 8, 13, 18, 23]),
+        get_clue(n, [4, 9, 14, 19, 24]),
     ]
 }
 
 fn main() {
-    let mut mem = HashMap::<Clues, u8>::with_capacity(1<<25);
+    let mut mem = HashMap::<Clues, u8>::with_capacity(1 << 25);
     for n in 0u32..(1 << 25) {
         *mem.entry(get_clues(n)).or_insert(0) += 1;
     }
@@ -128,7 +67,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{FixSizeClue, get_bit, get_clue};
+    use crate::get_bit;
 
     #[test]
     fn test_get_bit() {
@@ -136,39 +75,5 @@ mod tests {
         assert_eq!(get_bit(1, 0), true);
         assert_eq!(get_bit(2, 0), false);
         assert_eq!(get_bit(2, 1), true);
-    }
-
-    fn to_fix_size_clue(nums: Vec<u8>) -> FixSizeClue {
-        let mut data = [0u8; 5];
-        let mut len = 0;
-        for x in nums.iter() {
-            data[len] = *x;
-            len += 1;
-        }
-        FixSizeClue { len, data }
-    }
-
-    #[test]
-    fn test_get_clue() {
-        assert_eq!(
-            get_clue([false, false, false, false, false]),
-            to_fix_size_clue(vec![])
-        );
-        assert_eq!(
-            get_clue([false, false, false, false, true]),
-            to_fix_size_clue(vec![1])
-        );
-        assert_eq!(
-            get_clue([true, true, false, false, true]),
-            to_fix_size_clue(vec![2, 1])
-        );
-        assert_eq!(
-            get_clue([true, true, false, true, true]),
-            to_fix_size_clue(vec![2, 2])
-        );
-        assert_eq!(
-            get_clue([true, true, true, true, true]),
-            to_fix_size_clue(vec![5])
-        );
     }
 }
