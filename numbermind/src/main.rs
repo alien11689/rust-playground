abb2957f74f7bbb3086a5b8042d217da59b99ca2
Dominik Przybysz <alien11689@gmail.com>
2025-08-09 19:01:02 +0200

    Limit number of digits

--- numbermind/src/main.rs
+++ numbermind/src/main.rs
@@ -13,11 +13,14 @@ struct Args {
 
     #[arg(short, long, default_value_t = 10, value_parser = clap::value_parser!(u8).range(1..=30))]
     attempts: u8,
+
+    #[arg(short, long, default_value_t = 6, value_parser = clap::value_parser!(u8).range(1..=10))]
+    options: u8,
 }
 
-fn generate_random_code(seed: u64, length: u8) -> Vec<u8> {
+fn generate_random_code(seed: u64, length: u8, options: u8) -> Vec<u8> {
     let mut rng = StdRng::seed_from_u64(seed);
-    let random_code: Vec<u8> = (0..length).map(|_| rng.random_range(0..=9)).collect();
+    let random_code: Vec<u8> = (0..length).map(|_| rng.random_range(0..options)).collect();
     random_code
 }
 
@@ -89,7 +92,6 @@ fn vec_u8_to_string(digits: Vec<u8>) -> String {
     digits.iter().map(|d| d.to_string()).collect()
 }
 
-// todo limit number of digits
 // todo limit repetitions
 // todo add colors
 // todo calculate time spent
@@ -98,8 +100,14 @@ fn main() {
     let args = Args::parse();
     let seed = args.seed.unwrap_or_else(|| rand::rng().next_u64());
     let length = args.length;
-    let random_code = generate_random_code(seed, length);
-    println!("Seed: {}, Code length: {}", seed, length);
+    let options = args.options;
+    let random_code = generate_random_code(seed, length, options);
+    println!(
+        "Seed: {}, Code length: {}, digits: 0-{}",
+        seed,
+        length,
+        options - 1
+    );
     let mut i = 1u8;
     while i <= args.attempts {
         let result = read_input(format!("Guess the number (try {}/{}): ", i, args.attempts));
@@ -139,19 +147,22 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case(3567657657632322323, 4, vec![7, 3, 7, 8])]
-    #[case(343243242, 6, vec![6, 1, 5, 2, 4, 0])]
+    #[case(3567657657632322323, 4, vec![7, 3, 7, 8], 10)]
+    #[case(343243242, 6, vec![6, 1, 5, 2, 4, 0], 10)]
+    #[case(3567657657632322323, 4, vec![3, 1, 3, 4], 5)]
+    #[case(3567657657632322323, 6, vec![2, 1, 3, 3, 0, 1], 4)]
     fn test_random_number_generation(
         #[case] seed: u64,
         #[case] length: u8,
         #[case] expected: Vec<u8>,
+        #[case] options: u8,
     ) {
-        assert_eq!(generate_random_code(seed, length), expected);
+        assert_eq!(generate_random_code(seed, length, options), expected);
     }
 
     #[rstest]
-    #[case(vec!["name"], None, 4, 10)]
-    #[case(vec!["name", "-s", "233213", "-l", "6", "-a", "5"], Some(233213), 6, 5)]
+    #[case(vec!["name"], None, 4, 10, 6)]
+    #[case(vec!["name", "-s", "233213", "-l", "6", "-a", "5", "-o", "7"], Some(233213), 6, 5, 7)]
     #[case(vec![
             "name",
             "--seed",
@@ -160,12 +171,15 @@ mod tests {
             "6",
             "--attempts",
             "5",
-        ], Some(233213), 6, 5)]
+            "--options",
+            "7",
+        ], Some(233213), 6, 5, 7)]
     fn args_parsed_successfully(
         #[case] input: Vec<&str>,
         #[case] expected_seed: Option<u64>,
         #[case] expected_length: u8,
         #[case] expected_attempts: u8,
+        #[case] expected_options: u8,
     ) {
         let args = Args::parse_from(input);
         assert_eq!(
@@ -174,6 +188,7 @@ mod tests {
                 seed: expected_seed,
                 length: expected_length,
                 attempts: expected_attempts,
+                options: expected_options,
             }
         );
     }
@@ -185,6 +200,8 @@ mod tests {
     #[case(vec!["name", "-l", "11"])]
     #[case(vec!["name", "-a", "0"])]
     #[case(vec!["name", "-a", "31"])]
+    #[case(vec!["name", "-o", "0"])]
+    #[case(vec!["name", "-o", "11"])]
     fn reject_invalid_args(#[case] input: Vec<&str>) {
         let res = Args::try_parse_from(input);
         assert!(res.is_err())
